21378c838f49d304fb37ccd12ca75096fe188d98
trianto180 <43577042+trianto180@users.noreply.github.com>
2018-10-04 13:46:45 +0700

    Add files via upload

---
+++ Angga_17.11.1048/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Demo_Terbilang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.angka = new System.Windows.Forms.TextBox();
+            this.Terbilang = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check Terbilang";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ketik Nominal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 76);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Terbilang";
+            // 
+            // angka
+            // 
+            this.angka.Location = new System.Drawing.Point(108, 21);
+            this.angka.Margin = new System.Windows.Forms.Padding(2);
+            this.angka.Name = "angka";
+            this.angka.Size = new System.Drawing.Size(124, 20);
+            this.angka.TabIndex = 4;
+            this.angka.TextChanged += new System.EventHandler(this.angka_TextChanged);
+            // 
+            // Terbilang
+            // 
+            this.Terbilang.Location = new System.Drawing.Point(108, 73);
+            this.Terbilang.Margin = new System.Windows.Forms.Padding(2);
+            this.Terbilang.Name = "Terbilang";
+            this.Terbilang.Size = new System.Drawing.Size(387, 83);
+            this.Terbilang.TabIndex = 5;
+            this.Terbilang.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(527, 198);
+            this.Controls.Add(this.Terbilang);
+            this.Controls.Add(this.angka);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Demo Terbilang";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox angka;
+        private System.Windows.Forms.RichTextBox Terbilang;
+    }
+}
+
---
+++ Angga_17.11.1048/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassTerbilang;
+
+namespace Demo_Terbilang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nominal = int.Parse(angka.Text);
+            Class1 terbilang = new Class1();
+            Terbilang.Text = Convert.ToString(terbilang.TerbilangIndonesia(nominal));
+        }
+
+        private void angka_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
